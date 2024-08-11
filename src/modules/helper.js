1b42e0bdffe23b3d7a71532cc621326b32853d21
Allen Tan <tanallenbernard@gmail.com>
2024-08-11 16:46:55 +0800

    delete datetime.js and transfer functions to helper.js
    transfer date and time formatting functions to
    helper.js to be as one with other helper functions

--- src/modules/helper.js
+++ src/modules/helper.js
@@ -1,4 +1,19 @@
 /* eslint-disable no-console */
+import { format, parse } from 'date-fns';
+
+function formatTime(time) {
+  const parsedTime = parse(time, 'HH:mm:ss', new Date());
+  return format(parsedTime, 'HH:mm');
+}
+
+function formatDate(date) {
+  return format(date, 'EEE, dd MMM');
+}
+
+function getToday() {
+  const today = new Date();
+  return format(today, 'EEE, dd MMM');
+}
 
 function computeRainRate(precipType, precipProb) {
   const rainRate = !precipType === 'rain' ? '0%' : `${precipProb}%`;
@@ -17,7 +32,7 @@ function getHourlyData(data) {
         data.days[0].hours[i].precipprob,
       ),
       weatherIcon: data.days[0].hours[i].icon,
-      hour: data.days[0].hours[i].datetime,
+      hour: formatTime(data.days[0].hours[i].datetime),
     };
     hours.push(hour);
   }
@@ -29,7 +44,7 @@ function getDailyData(data) {
 
   for (let i = 0; i < 15; i += 1) {
     const day = {
-      date: data.days[i].datetime,
+      date: formatDate(data.days[i].datetime),
       rainRate: computeRainRate(
         data.days[i].preciptype,
         data.days[i].precipprob,
@@ -43,4 +58,11 @@ function getDailyData(data) {
   return days;
 }
 
-export { computeRainRate, getHourlyData, getDailyData };
+export {
+  formatTime,
+  formatDate,
+  getToday,
+  computeRainRate,
+  getHourlyData,
+  getDailyData,
+};
