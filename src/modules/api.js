3fc0b937b96664a21dce55d1d2d3257208703930
Allen Tan <tanallenbernard@gmail.com>
2024-08-14 07:05:47 +0800

    modify weatherData object
    export weatherData and dynamically set its values
    from segregateData

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+import setPageFlow from './dom';
 import {
   getHourlyData,
   getDailyData,
@@ -7,36 +8,34 @@ import {
   getToday,
 } from './helper';
 
+const weatherData = {};
+
 function segregateData(data) {
-  console.log(data);
-
-  const weatherData = {
-    todayInfo: {
-      currentTemp: data.currentConditions.temp,
-      currentCondIcon: data.currentConditions.icon,
-      currentCond: data.currentConditions.conditions,
-      currentLocation: data.resolvedAddress,
-      currentTime: formatTime(data.currentConditions.datetime),
-      currentDate: getToday(),
-    },
-    todayCondition: {
-      feelsLike: data.currentConditions.feelslike,
-      rainRate: computeRainRate(
-        data.currentConditions.preciptype,
-        data.currentConditions.precipprob,
-      ),
-      humidity: data.currentConditions.humidity,
-      uvIndex: data.currentConditions.uvindex,
-      visibility: data.currentConditions.visibility,
-      pressure: data.currentConditions.pressure,
-      sunrise: formatTime(data.currentConditions.sunrise),
-      sunset: formatTime(data.currentConditions.sunset),
-    },
-    hourlyForecast: getHourlyData(data),
-    dailyForecast: getDailyData(data),
+  weatherData.todayInfo = {
+    currentTemp: data.currentConditions.temp,
+    currentCondIcon: data.currentConditions.icon,
+    currentCond: data.currentConditions.conditions,
+    currentLocation: data.resolvedAddress,
+    currentTime: formatTime(data.currentConditions.datetime),
+    currentDate: getToday(),
   };
+  weatherData.todayCondition = {
+    feelsLike: data.currentConditions.feelslike,
+    rainRate: computeRainRate(
+      data.currentConditions.preciptype,
+      data.currentConditions.precipprob,
+    ),
+    humidity: data.currentConditions.humidity,
+    uvIndex: data.currentConditions.uvindex,
+    visibility: data.currentConditions.visibility,
+    pressure: data.currentConditions.pressure,
+    sunrise: formatTime(data.currentConditions.sunrise),
+    sunset: formatTime(data.currentConditions.sunset),
+  };
+  weatherData.hourlyForecast = getHourlyData(data);
+  weatherData.dailyForecast = getDailyData(data);
 
-  console.log(weatherData);
+  setPageFlow(weatherData);
 }
 
 async function getWeatherData(url) {
@@ -55,4 +54,4 @@ async function getWeatherData(url) {
   }
 }
 
-export { segregateData, getWeatherData };
+export { weatherData, segregateData, getWeatherData };
