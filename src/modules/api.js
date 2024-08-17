7d30b1cf5299d8b22ad917f54592d91287362847
Allen Tan <tanallenbernard@gmail.com>
2024-08-17 10:37:22 +0800

    utilize loadingScreen and errorScreen for await and catching error

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,18 +1,20 @@
-/* eslint-disable no-console */
+import { createErrorScreen, createLoadingScreen } from './dom';
 import { rawWeatherData } from './weatherData';
 
 export default async function getWeatherData(url, segregateData) {
   try {
     const response = await fetch(url);
+    createLoadingScreen();
 
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      throw new Error(
+        `Location not found. Please ensure you include the city/country name in your input`,
+      );
     }
 
     Object.assign(rawWeatherData, await response.json());
     segregateData();
   } catch (error) {
-    console.log(`ERROR: ${error}`);
-    // display something when error
+    createErrorScreen(error);
   }
 }
